ecca127ce02dc589304633a70bf5a91b11729a76
Lewis Larsen <lewis@larsens.dev>
2025-05-01 18:52:23 +0100

    wip

---
+++ app/Actions/Notifications/SendWebhookNotification.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Notifications;
+
+use App\Models\Video;
+use Exception;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class SendWebhookNotification
+{
+    public function execute(Video $video): bool
+    {
+        $webhookUrl = config('app.webhook_post_url');
+
+        if (empty($webhookUrl)) {
+            return false;
+        }
+
+        try {
+            $externalRequest = Http::post($webhookUrl, [
+                'title' => $video->getAttribute('title'),
+                'video_url' => $video->getYoutubeUrl(),
+                'thumbnail' => $video->getThumbnailUrl(),
+                'published_at' => $video->getAttribute('published_at')->toDateTimeString(),
+                'published_at_formatted' => $video->getFormattedPublishedDate(),
+                'channel' => [
+                    'label' => $video->channel->getAttribute('name'),
+                    'url' => $video->channel->getChannelUrl(),
+                ],
+            ]);
+
+            if ($externalRequest->failed()) {
+                Log::error('An error occurred while sending the webhook notification.', [
+                    'video_id' => $video->id,
+                    'response' => $externalRequest->body(),
+                ]);
+
+                return false;
+            }
+
+            Log::debug('Webhook notification sent.', [
+                'video_id' => $video->id,
+                'response' => $externalRequest->body(),
+            ]);
+
+            return $externalRequest->successful();
+
+        } catch (RequestException $e) {
+            Log::error('An error occurred while sending the webhook notification.', [
+                'video_id' => $video->id,
+                'response' => $e->response->body(),
+                'exception' => $e->getMessage(),
+            ]);
+
+            return false;
+        } catch (Exception $e) {
+            Log::error('An error occurred while sending the webhook notification.', [
+                'video_id' => $video->id,
+                'exception' => $e->getMessage(),
+            ]);
+
+            return false;
+        }
+    }
+}
