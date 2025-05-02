71439678d912d3ddfe7786ef3d48358069102b0d
Lewis Larsen <lewis@larsens.dev>
2025-05-02 17:04:27 +0100

    fixed email template

--- resources/views/mail/test-mail.blade.php
+++ resources/views/mail/test-mail.blade.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 ?>
 <x-mail::message>
-# Notification email test
+## Notification email test
 
 If you are receiving this email, your connection to your SMTP provider is working correctly.
 </x-mail::message>
